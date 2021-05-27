bb66a09f03b9b95e4d169a7a5a5470d243e89006
Laurie Lonecrow <laurieteresa@gmail.com>
2021-05-27 07:08:54 -0400

    Initial Commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import styled from 'styled-components/macro';
+
+import {
+  COLORS,
+  WEIGHTS
+} from '../../constants';
+import Logo from '../Logo';
+import SuperHeader from '../SuperHeader';
+
+const Header = () => {
+  // Our site features two visual headers, but they should be
+  // grouped semantically as a single header.
+  return ( <
+    header >
+    <
+    SuperHeader / >
+    <
+    MainHeader >
+    <
+    Logo / >
+    <
+    Nav >
+    <
+    NavLink href = "/sale" > Sale < /NavLink> <
+    NavLink href = "/new" > New Releases < /NavLink> <
+    NavLink href = "/men" > Men < /NavLink> <
+    NavLink href = "/women" > Women < /NavLink> <
+    NavLink href = "/kids" > Kids < /NavLink> <
+    NavLink href = "/collections" > Collections < /NavLink> < /
+    Nav > <
+    /MainHeader> < /
+    header >
+  );
+};
+
+const MainHeader = styled.div `
+  display: flex;
+  justify-content: space-between;
+  padding: 32px;
+  border-bottom: 1px solid ${COLORS.gray[300]};
+`;
+
+const Nav = styled.nav `
+display: flex;
+justify-content: center;
+margin-left: auto;
+margin-right: auto;
+`;
+
+const NavLink = styled.a `
+  font-size: 1.125rem;
+  text-transform: uppercase;
+  text-decoration: none;
+  color: ${COLORS.gray[900]};
+  font-weight: ${WEIGHTS.medium};
+  padding: 0 24px;
+
+  &:first-of-type {
+    color: ${COLORS.secondary};
+  }
+`;
+
+export default Header;
